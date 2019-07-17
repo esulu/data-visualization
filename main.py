f941c92733fc0510340f3315014f04bb625a2bd5
Eren Sulutas <erensulu11@gmail.com>
2019-07-17 13:50:08 -0400

    working build with sample data

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
+import numpy as np
+
 from bokeh.plotting import figure, output_file, show
 import pandas
 
-# Read in csv
-data_frame = pandas.read_csv('ontario-data.csv')
+def datetime(x):
+    return np.array(x, dtype=np.datetime64)
 
-field = data_frame["Field of study"]
-year_14 = data_frame["2014 / 2015"]
+# Read in csv
+data_frame = pandas.read_csv('sample.csv')
+
+##field = data_frame["field"]
+date = data_frame['date']
+cost = data_frame['cost']
 
 output_file('index.html', title='example data')
 
 # Add plot
 p = figure (
+    x_range=date,
     plot_width = 800,
     plot_height= 600,
     title = "Plot Title",
-    x_axis_label = "X Axis",
-    y_axis_label= "Y Axis",
+    x_axis_label = "Date",
+    y_axis_label= "Cost",
     tools=""
 )
 
 # Render glyph
-p.line(x, y, legend="Test", line_width=2)
+#p.line(x, y, legend="Field 1", line_width=2)
+#p.line(date, cost, legend = "field")
+p.line(
+    y=cost,
+    x=date
+)
 
 # Show results 
 show(p)
