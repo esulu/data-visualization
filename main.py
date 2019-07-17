19a72afa5dba91db23ad326643a9b193defa834f
Eren Sulutas <erensulu11@gmail.com>
2019-07-17 12:49:52 -0400

    in progress

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from bokeh.plotting import figure, output_file, show
+import pandas
 
-x = [1, 2, 3, 4, 5]
-y = [4, 6, 2, 4, 2]
+# Read in csv
+data_frame = pandas.read_csv('ontario-data.csv')
 
-output_file('index.html')
+field = data_frame["Field of study"]
+year_14 = data_frame["2014 / 2015"]
+
+output_file('index.html', title='example data')
 
 # Add plot
 p = figure (
+    plot_width = 800,
+    plot_height= 600,
     title = "Plot Title",
     x_axis_label = "X Axis",
-    y_axis_label= "Y Axis"
+    y_axis_label= "Y Axis",
+    tools=""
 )
 
 # Render glyph
