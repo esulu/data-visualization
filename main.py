adaf534bb5727264c114b566334d890fb74437e4
Eren Sulutas <erensulu11@gmail.com>
2019-07-17 12:20:55 -0400

    basic chart

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ from bokeh.plotting import figure, output_file, show
 x = [1, 2, 3, 4, 5]
 y = [4, 6, 2, 4, 2]
 
+output_file('index.html')
+
+# Add plot
+p = figure (
+    title = "Plot Title",
+    x_axis_label = "X Axis",
+    y_axis_label= "Y Axis"
+)
+
+# Render glyph
+p.line(x, y, legend="Test", line_width=2)
+
+# Show results 
+show(p)
