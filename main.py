898c159a868bb0194878abaded748807cdd37bd8
Eren Sulutas <erensulu11@gmail.com>
2019-07-19 10:36:48 -0400

    optimizing code

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import numpy as np
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, output_file, show, ColumnDataSource
 from bokeh.models import ColumnDataSource
 import pandas
 
 # Read in csv
 data_frame = pandas.read_csv('ontario-data.csv')
 
-'''field = data_frame['field']
-date = data_frame['date']
-cost = data_frame['cost']'''
+# Create ColumnDataSource from the data frame
+source = ColumnDataSource(data_frame)
+
+data_list = source.data['Field of study'].tolist()
+cost_list = source.data['2014 / 2015'].tolist()
+
+print(data_list)
+print(cost_list)
 
 l = []
 
@@ -17,20 +22,6 @@ for i in range(1, len(data_frame)):
     l.append(data_frame.iloc[i])
 
 dates = ["2014 / 2015","2015 / 2016","2016 / 2017","2017 / 2018","2018 / 2019"]
-
-'''# List of all the fields 
-l = list(data_frame['Field of study'])
-l.pop(0)
-l.pop(0)
-
-print(data_frame.iloc[1])'''
-
-'''data = data_frame.iloc[3]
-cost = data["2014 / 2015"]
-
-print(data)
-print(cost)'''
-
 
 output_file('index.html', title='temp')
 
@@ -46,20 +37,7 @@ p = figure (
     tools="pan,wheel_zoom,save,reset"
 )
 
-'''p = figure (
-    x_range=date,
-    plot_width = 800,
-    plot_height= 600,
-    title = "Plot Title",
-    x_axis_label = "Date",
-    y_axis_label= "Cost",
-    tools=""
-)'''
-
 # Render glyph
-#p.line(x, y, legend="Field 1", line_width=2)
-#p.line(date, cost, legend = "field")
-
 
 #p = figure(width=500, height=300, x_axis_type="datetime") 
 
@@ -69,42 +47,22 @@ cost = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
 for i in range(data_size):
     for j in range(1, 6):
         cost[i].append(l[i][j])
-    #print(l[i][0])
-    #print(cost[i])
-    '''p.line(
-        y=cost,
-        x=dates,
-        legend=str(i)
-    )
-    cost.clear()'''
 
 #cost = ['6481', '6658', '6695', '6826', '7016']
 
-for i in range(data_size):
-    print(l[i][0])
-    print(cost[i])
+'''for i in range(data_size):
     p.line(
         y=cost[i],
         x=dates,
         legend=l[i][0]
-    )
+    )'''
 
-'''p.line(
-    y=cost,
-    x=dates,
-    legend="line"
-)'''
-'''p.line(
-    y=cost,
-    x=date,
-    legend="total"
-)
 p.line(
-    y=cost,
-    x=date,
-    legend="education"
-)'''
-
-
+        y='Field of study',
+        x='2014 / 2015',
+        #legend=l[i][0],
+        legend='temp',
+        source=source
+    )
 # Show results 
 show(p)
