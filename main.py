22bd9484c5a803b5547b2d882bf9d36da2f5f4f9
Eren Sulutas <erensulu11@gmail.com>
2019-07-20 10:48:37 -0400

    final working build

--- main.py
+++ main.py
@@ -1,39 +1,29 @@
 import numpy as np
 from bokeh.plotting import figure, output_file, show, save, ColumnDataSource
 from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.palettes import Category20
 import pandas
 
 # Read in csv
-df = pandas.read_csv('sample.csv')
+df = pandas.read_csv('ontario-data.csv')
 
 # Create ColumnDataSource from the data frame
 source = ColumnDataSource(df)
 
-field_list = source.data['field'].tolist()
-cost_list = source.data['cost'].tolist()
-date_list = source.data['date'].tolist()
+field = source.data['field']
+cost = source.data['cost']
+date = source.data['date']
 
-# Elements in each list
-n = 5
+field_list = df.field.unique()
 
-final_cost = [cost_list[i * n:(i + 1) * n] for i in range((len(cost_list) + n - 1) // n )]  
-final_date = [date_list[i * n:(i + 1) * n] for i in range((len(date_list) + n - 1) // n )]  
-
-'''print(data_list)
-print(cost_list)
-print(date_list)'''
-
-'''l = []
-
-# Makes a list of data for each field
-for i in range(1, len(df)): 
-    l.append(df.iloc[i])'''
-
+#dates = date.tolist()
 dates = ["2014 / 2015","2015 / 2016","2016 / 2017","2017 / 2018","2018 / 2019"]
 
-output_file('index.html', title='temp')
+'''dates_list = dates.tolist()
+date_set = set(dates)
+dates = list(date_set)'''
 
-# Add plot
+output_file('index.html', title='temp')
 
 p = figure (
     x_range=dates,
@@ -42,36 +32,36 @@ p = figure (
     title = "Plot Title",
     x_axis_label = "Date",
     y_axis_label= "Cost",
-    tools="pan,wheel_zoom,save,reset"
+    #tools="pan,wheel_zoom,save,reset"
 )
 
-# Render glyph
+# Add plot
+for i in range(len(field_list)):
+    source2 = ColumnDataSource(
+         data={'date':df.loc[df.field == field_list[i]].date,
+            'field':df.loc[df.field == field_list[i]].field,
+            'cost':df.loc[df.field == field_list[i]].cost}
+    )
+
+    # Render glyph
+    p.line(
+        x='date',
+        y='cost',
+        source=source2,
+        #legend = field_list[i],
+        color = (Category20[len(field)/5])[i]
+    )
+
+
 
 #p = figure(width=500, height=300, x_axis_type="datetime") 
 
-'''
-data_size = len(l)-1
-cost = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
-
-for i in range(data_size):
-    for j in range(1, 6):
-        cost[i].append(l[i][j])
-
-#cost = ['6481', '6658', '6695', '6826', '7016']
-
-for i in range(data_size):
-    p.line(
-        y=cost[i],
-        x=dates,
-        legend=l[i][0]
-    )'''
-
 # Problem: Making one line, so it connects all sections
-p.line(
+'''p.line(
         y='cost',
         x='date',
         source=source
-    )
+    )'''
 
 # Problem: Multi line required a list of lists for the xs and ys
 '''p.multi_line(
